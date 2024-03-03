07c57d065cb843736dcea494820ade10b3918e16
emnificent <49172724+emnificent@users.noreply.github.com>
2024-03-03 02:01:31 +0100

    fix: minor Commentary improvements

--- src/lib/components/Commentary.svelte
+++ src/lib/components/Commentary.svelte
@@ -1,7 +1,7 @@
 <div>
-  <p>I buy domains for my projects, but sometimes I change project names or abandon them, resulting in expiring domains. Too often, speculators acquire these domains at expiration and lists them for exorbitant prices. Thus, I now try to sell my expiring domains to give others a chance to get their desired domain for their projects.</p>
-  <p>While I may not always list my domains at low prices, as I need to recoup what I spent on domains I let expire somehow, I'm open to negotiate. If you have an interesting project idea and you're on a tight budget, tell me about it on NamePros, and if I like your idea, I'll offer you a discounted price and we'll proceed through Escrow.</p>
-  <p>Or you could gamble on the chance that speculators won't acquire the domain at expiration and try to secure it then for a lower price. But too many times I've been disappointed of seeing a domain I wanted being registered by them before I had a chance to secure it myself. Now, those domains are priced way too high.</p>
+  <p>I buy domains for my projects, but sometimes I change project names or abandon them, resulting in expiring domains. Too often, speculators acquire these domains at expiration and list them for exorbitant prices. Thus, I now try to sell my expiring domains to give others a chance to get their desired domain for their projects.</p>
+  <p>While I may not always list my domains cheaply, as I need to recoup what I spent on domains I let expire, I'm open to negotiate. If you have an interesting project idea and you're on a tight budget, tell me about it on NamePros, and if I like your idea, I'll offer you a discounted price and we'll proceed through Escrow.</p>
+  <p>Or you could gamble on the chance that speculators won't acquire the domain at expiration and try to secure it then for a lower price. Though I never was able to secure the domains I wanted before speculators registered them. Now, those domains are priced way too high.</p>
   <p>Example of a domain name speculator: HugeDomains</p>
 </div>
 
