270d320b20cc83e8ed9cb90db62b6b908c470e5c
emnificent <49172724+emnificent@users.noreply.github.com>
2024-04-21 12:25:33 +0200

    feat: add auto-renew disclaimer

--- src/lib/components/Commentary.svelte
+++ src/lib/components/Commentary.svelte
@@ -1,13 +1,25 @@
 <div>
   <p>I buy domains for my projects, but sometimes I change project names or abandon them, resulting in expiring domains. Too often, speculators acquire these domains at expiration and list them for exorbitant prices. Thus, I now try to sell my expiring domains to give others a chance to secure their desired domain for their projects while they still can.</p>
+  <p class="disclaimer">Disclaimer: <a href="https://lustfulslut.com">lustfulslut.com</a> and <a href="https://lustess.com">lustess.com</a> are set to auto-renew.</p>
   <p>While my listings may be expensive, as I need to recoup what I spent on domains I let expire, I'm open to negotiate. If you have an interesting project idea and you're on a tight budget, send me a message, and if I like your idea, I'll offer you a discounted price and we'll proceed through escrow. I rather see a domain in use than on a speculator's store.</p>
   <p>Or you could gamble on the chance that speculators won't acquire the domain at expiration and try to register it then for a lower price. Though I never was able to register the expiring domains I wanted to snatch before speculators did, and now those domains are priced way too high. My advice would be to secure your domain now, but you do you.</p>
 </div>
 
-<style>
+<style lang="scss">
   div {
     display: flex;
     flex-direction: column;
     gap: 1rem;
+  }
+
+  .disclaimer {
+    padding: 0.5rem 0.75rem;
+    border-radius: 0.5rem;
+    background-color: var(--c-dark--deeper);
+    font-size: var(--fs-text--small);
+
+    @media (prefers-color-scheme: light) {
+      background-color: var(--c-light--lighter);
+    }
   }
 </style>
