271b66096a81a46b6a35b746b8c702e2e3b93096
emnificent <49172724+emnificent@users.noreply.github.com>
2024-04-21 13:05:38 +0200

    feat: make domains list collapsable on mobile and tablet

--- src/lib/components/Domains.svelte
+++ src/lib/components/Domains.svelte
@@ -16,22 +16,35 @@
   onMount(() => {
     filteredDomainsList = domainsList.filter(element => element.name !== domain);
   })
+
+  let closed = true;
+  function expandCollapseList() {
+    closed = !closed
+  }
 </script>
 
 <section>
-  <h2>Other domains for sale ({filteredDomainsList.length})</h2>
-  <ul id="domains-list">
-    { #each filteredDomainsList as domain }
-      <li><a href="https://{domain.name}" target="_blank">{domain.name}</a></li>
-    { /each }
-  </ul>
+  <div class="list__header">
+    <label for="dropdown-button">
+      <h2>Other domains for sale ({filteredDomainsList.length})</h2>
+    </label>
+
+    <input type="checkbox" id="dropdown-button" class="dropdown-button" aria-label="expand/collapse list" on:change={expandCollapseList}>
+  </div>
+
+  <div class="list-content__wrapper" class:closed>
+    <ul id="domains-list" class="list-content">
+      { #each filteredDomainsList as domain }
+        <li><a href="https://{domain.name}" target="_blank">{domain.name}</a></li>
+      { /each }
+    </ul>
+  </div>
 </section>
 
-<style>
+<style lang="scss">
   h2 {
     font-weight: var(--fw--bold);
     font-size: var(--fs-title--small);
-    margin-bottom: 1rem;
   }
 
   ul {
@@ -43,5 +56,77 @@
 
   a {
     font-family: monospace;
+  }
+
+  .list__header {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+
+    @media screen and (width >= 1280px) {
+      & input {
+        display: none;
+      }
+    }
+  }
+
+  /* list dropdown animation */
+  .list-content__wrapper {
+    display: grid;
+    grid-template-rows: 1fr;
+    transition: all 0.3s;
+
+    &.closed {
+      grid-template-rows: 0fr;
+    }
+
+    @media screen and (width >= 1280px) {
+      display: contents;
+    }
+  }
+  .list-content {
+    overflow: hidden;
+  }
+
+  .dropdown-button {
+    appearance: none;
+    position: relative;
+    height: 2rem;
+    width: 2rem;
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      translate: 0 -50%;
+      height: 20px;
+      width: 4px;
+      border-radius: 2px;
+      background-color: var(--c-light);
+      transition: rotate 0.3s;
+    }
+
+    &::before {
+      left: 62%;
+      rotate: -45deg;
+    }
+
+    &::after {
+      right: 62%;
+      rotate: 45deg;
+    }
+
+    &:checked {
+      &::before {
+        rotate: 45deg;
+      }
+
+      &::after {
+        rotate: -45deg;
+      }
+    }
   }
 </style>
