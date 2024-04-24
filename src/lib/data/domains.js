fd36fc33043d0472bed30041c634d2266087ca77
emnificent <49172724+emnificent@users.noreply.github.com>
2024-04-24 10:10:34 +0200

    chore: price update

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -4,11 +4,11 @@ const domains = [
   { name: 'antifapost.com', price: 198 },
   { name: 'blackcats.party', price: 198 },
   { name: 'bum.su', price: 742 },
+  { name: 'e-pals.party', price: 66 },
   { name: 'emilie.fun', price: 132 },
   { name: 'emilie.pm', price: 292 },
-  { name: 'e-pals.party', price: 66 },
   { name: 'lust.sx', price: 992 },
-  { name: 'lustess.com', price: 990 },
+  { name: 'lustess.com', price: 1979 },
   { name: 'lustfulslut.com', price: 1979 },
   { name: 'struggleunion.com', price: 198 },
   { name: 'testtube.work', price: 66 }
