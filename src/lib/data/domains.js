eda69a21d282fde7c5febd4b9a8eeda599f0bb91
emnificent <49172724+emnificent@users.noreply.github.com>
2024-04-27 01:48:19 +0200

    chore: update prices

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -1,17 +1,18 @@
 const domains = [
-  { name: 'anaranar.com', price: { sedo: 396, dan: 431 } },
+  { name: 'anaranar.com', price: { sedo: 198, dan: 216 } },
   { name: 'antifamail.org', price: { sedo: 198, dan: 216 } },
   { name: 'antifapost.com', price: { sedo: 198, dan: 216 } },
   { name: 'blackcats.party', price: { sedo: 198, dan: 216 } },
-  { name: 'bum.su', price: { sedo: 742, dan: 810 } },
+  { name: 'bum.su', price: { sedo: 742, dan: 717 } },
   { name: 'e-pals.party', price: { sedo: 66, dan: 100 } },
   { name: 'emilie.fun', price: { sedo: 132, dan: 144 } },
-  { name: 'emilie.pm', price: { sedo: 292, dan: 319 } },
-  { name: 'lust.sx', price: { sedo: 992, dan: 1083 } },
-  { name: 'lustess.com', price: { sedo: 1979, dan: 2312 } },
-  { name: 'lustfulslut.com', price: { sedo: 1979, dan: 2312 } },
+  { name: 'emilie.pm', price: { sedo: 292, dan: 100 } },
+  { name: 'lust.sx', price: { sedo: 1498, dan: 1628 } },
+  { name: 'lustess.com', price: { sedo: 3299, dan: 3853 } },
+  { name: 'lustfulslut.com', price: { sedo: 3299, dan: 3853 } },
   { name: 'struggleunion.com', price: { sedo: 198, dan: 216 } },
   { name: 'testtube.work', price: { sedo: 66, dan: 100 } },
+  // useful when coding
   { name: 'localhost', price: { sedo: 20, dan: 100 } }
 ];
 
