ffc81435449e0061f7a888f498fea9269f4cf62a
emnificent <49172724+emnificent@users.noreply.github.com>
2024-04-27 19:50:49 +0200

    chore: update domains list

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -8,7 +8,6 @@ const domains = [
   { name: 'emilie.fun', price: { sedo: 99, dan: 108 } },
   { name: 'emilie.pm', price: { sedo: 262, dan: 100 } },
   { name: 'lust.sx', price: { sedo: 1498, dan: 1628 } },
-  { name: 'lustess.com', price: { sedo: 3299, dan: 3853 } },
   { name: 'lustfulslut.com', price: { sedo: 3299, dan: 3853 } },
   { name: 'struggleunion.com', price: { sedo: 99, dan: 108 } },
   { name: 'testtube.work', price: { sedo: 40, dan: 100 } },
