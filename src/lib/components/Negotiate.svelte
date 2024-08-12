8373a529a943f5e1d475672b7b5e77818f697a35
emnificent <49172724+emnificent@users.noreply.github.com>
2024-08-12 17:10:27 +0200

    feat: replace the contact form with the NamePros landing

--- src/lib/components/Negotiate.svelte
+++ src/lib/components/Negotiate.svelte
@@ -1,37 +1,13 @@
 <script>
-  import { slide } from 'svelte/transition';
-
   import domainStore from '$lib/store/domainStore.js';
   $: domainName = $domainStore;
-  
-  export let form;
-
-  let displayForm = false;
-  function revealForm() {
-    displayForm = true;
-  }
 </script>
 
 <section>
   <h2>Negotiate</h2>
-  <p>Tight budget? No sweat, shoot me your project idea and your price point. If it tickles my fancy, we can talk discount! <span class="mobile-delete"><br /></span> But hey, this domain's a gem, remember that.</p>
+  <p>Tight budget? No sweat, shoot me your project idea and your price point. If it tickles my fancy, we can talk discount! <span class="mobile-delete"><br /></span> Rumors say some even got their domain transfer for free?!</p>
 
-  { #if displayForm }
-    <form method="POST" transition:slide={{ duration: 300 }}>
-      <input type="text" name="domain" value={domainName} readonly tabindex="-1" aria-hidden="true" class="screenreaders-only" />
-      <input type="email" name="email" placeholder="name@email.com" required />
-      <textarea name="message" rows="5" placeholder="Describe your project and specify your budget" required></textarea>
-      <button>Send me a message</button>
-    </form>
-  { :else if !form }
-    <button on:click={revealForm}>Let's talk</button>
-  { :else }
-    { #if form?.success }
-      <p class="message success">Thanks for your message!</p>
-    { :else if form?.error }
-      <p class="message error">There was an error, try again later.</p>
-    { /if }
-  { /if }
+  <a href="https://www.namepros.com/parked/{domainName}" target="_blank">Let's talk</a>
 </section>
 
 <style lang="scss">
@@ -60,7 +36,7 @@
     }
   }
 
-  button {
+  a {
     border: 2px solid transparent;
     width: fit-content;
     align-self: center;
@@ -79,61 +55,6 @@
 
     &:hover {
       border-color: var(--c-primary);
-    }
-  }
-
-  form {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-
-    & textarea,
-    & input,
-    & button {
-      padding: 8px 16px;
-      border-radius: 8px;
-    }
-
-    & textarea,
-    & input {
-      background-color: var(--c-background--bold);
-      color: var(--c-text);
-      border: none;
-
-      &::placeholder {
-        color: var(--c-text);
-        opacity: 0.5;
-      }
-
-      &:focus-visible {
-        outline: var(--c-primary) 2px solid;
-        border-radius: 8px;
-      }
-    }
-
-    & textarea {
-      resize: vertical;
-    }
-
-    & button {
-      align-self: flex-end;
-    }
-  }
-
-  .message {
-    padding: 8px 16px;
-    border-radius: 8px;
-    color: var(--c-background--bold);
-    font-weight: var(--fw--bold);
-    width: fit-content;
-    align-self: center;
-
-    &.success {
-      background-color: var(--c-success);
-    }
-
-    &.error {
-      background-color: var(--c-error);
     }
   }
 </style>
