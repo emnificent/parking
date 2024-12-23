dcfc3690ae52dc6eb3231960c561628c7ff3ba9e
emnificent <49172724+emnificent@users.noreply.github.com>
2024-12-24 00:21:21 +0100

    chore: update domains list

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -2,7 +2,6 @@ const domains = [
   { name: 'bum.su', price: { sedo: 742, dynadot: 0 } },
   { name: 'cropdinate.com', price: { sedo: 66, dynadot: 100 } },
   { name: 'emilie.fun', price: { sedo: 99, dynadot: 100 } },
-  { name: 'emilie.pm', price: { sedo: 262, dynadot: 0 } },
   { name: 'lustfulslut.com', price: { sedo: 3272, dynadot: 2822 } },
   { name: 'pneumatube.com', price: { sedo: 66, dynadot: 100 } },
   { name: 'satancore.com', price: { sedo: 99, dynadot: 100 } },
