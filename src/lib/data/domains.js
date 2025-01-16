0debfec6d540cc8b2f291d5d111e47bd0fa2fd71
emnificent <49172724+emnificent@users.noreply.github.com>
2025-01-17 00:45:48 +0100

    chore: update domains list

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -1,6 +1,7 @@
 const domains = [
   { name: 'cropdinate.com', price: { sedo: 66, dynadot: 100 } },
   { name: 'lustfulslut.com', price: { sedo: 3272, dynadot: 2822 } },
+  { name: 'lustpad.com', price: { sedo: 1023, dynadot: 882 } },
   { name: 'pneumatube.com', price: { sedo: 66, dynadot: 100 } },
   { name: 'satancore.com', price: { sedo: 99, dynadot: 100 } },
 ];
