e31c694159d20ea2cc57a9752df5c7e55d93c74e
emnificent <49172724+emnificent@users.noreply.github.com>
2025-01-20 17:41:37 +0100

    chore: update domains list

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -1,9 +1,9 @@
 const domains = [
-  { name: 'cropdinate.com', price: { sedo: 66, dynadot: 100 } },
-  { name: 'lustfulslut.com', price: { sedo: 3272, dynadot: 2822 } },
-  { name: 'lustpad.com', price: { sedo: 1023, dynadot: 882 } },
-  { name: 'melaphos.com', price: { sedo: 594, dynadot: 512 } },
-  { name: 'pneumatube.com', price: { sedo: 66, dynadot: 100 } },
+  { name: 'cropdinate.com', price: { sedo: 33, dynadot: 100 } },
+  { name: 'lustfulslut.com', price: { sedo: 2850, dynadot: 2458 } },
+  { name: 'lustpad.com', price: { sedo: 1425, dynadot: 1229 } },
+  { name: 'melaphos.com', price: { sedo: 297, dynadot: 256 } },
+  { name: 'pneumatube.com', price: { sedo: 33, dynadot: 100 } },
   { name: 'satancore.com', price: { sedo: 99, dynadot: 100 } },
 ];
 
