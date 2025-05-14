e1c1873bc0954bae3f12039605aaf03b5fb2ecf1
emnificent <49172724+emnificent@users.noreply.github.com>
2025-05-14 12:42:24 +0200

    chore: update domains list

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -1,15 +1,15 @@
 const domains = [
-  { name: 'breadfight.com', price: { sedo: 1650, dynadot: 1423 } },
-  { name: 'cropdinate.com', price: { sedo: 33, dynadot: 100 } },
-  { name: 'etheress.com', price: { sedo: 165, dynadot: 143 } },
-  { name: 'halfmymoney.com', price: { sedo: 1425, dynadot: 1229 } },
-  { name: 'lustfulslut.com', price: { sedo: 2850, dynadot: 2458 } },
-  { name: 'melaphos.com', price: { sedo: 297, dynadot: 256 } },
-  { name: 'musiquefatale.com', price: { sedo: 132, dynadot: 114 } },
-  { name: 'mystess.com', price: { sedo: 330, dynadot: 285 } },
-  { name: 'operess.com', price: { sedo: 33, dynadot: 100 } },
-  { name: 'pneumatube.com', price: { sedo: 33, dynadot: 100 } },
-  { name: 'satancore.com', price: { sedo: 99, dynadot: 100 } },
+  { name: 'breadfight.com', price: { sedo: 6267, dynadot: 5404 } },
+  { name: 'cropdinate.com', price: { sedo: 21373, dynadot: 18431 } },
+  { name: 'etheress.com', price: { sedo: 22692, dynadot: 19568 } },
+  { name: 'halfmymoney.com', price: { sedo: 3694, dynadot: 3186 } },
+  { name: 'lustfulslut.com', price: { sedo: 5541, dynadot: 4779 } },
+  { name: 'melaphos.com', price: { sedo: 1023, dynadot: 882 } },
+  { name: 'musiquefatale.com', price: { sedo: 2111, dynadot: 1821 } },
+  { name: 'mystess.com', price: { sedo: 15304, dynadot: 13197 } },
+  { name: 'operess.com', price: { sedo: 15568, dynadot: 13425 } },
+  { name: 'pneumatube.com', price: { sedo: 21373, dynadot: 18431 } },
+  { name: 'satancore.com', price: { sedo: 11610, dynadot: 10012 } },
 ];
 
 export default domains;
