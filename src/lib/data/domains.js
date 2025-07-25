e3971bd103bfb431b1a1fffa52dbbb506561fd78
emnificent <49172724+emnificent@users.noreply.github.com>
2025-07-25 14:45:18 +0200

    chore: update domain listings

--- src/lib/data/domains.js
+++ src/lib/data/domains.js
@@ -4,12 +4,10 @@ const domains = [
   { name: 'etheress.com', price: 2585 },
   { name: 'halfmymoney.com', price: 425 },
   { name: 'lustfulslut.com', price: 636 },
-  { name: 'melaphos.com', price: 119 },
   { name: 'musiquefatale.com', price: 251 },
   { name: 'mystess.com', price: 1746 },
   { name: 'operess.com', price: 1779 },
   { name: 'pneumatube.com', price: 2435 },
-  { name: 'unbowedpress.com', price: 936 },
 ];
 
 export default domains;
